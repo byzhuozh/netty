9d6e057b5a66bbe9c265281bc1fe2afb9f732278
weiqi <Abc123456>
2019-07-04 16:46:19 +0800

    channel、channelId、channelConfig 分析

--- transport/src/main/java/io/netty/channel/ChannelId.java
+++ transport/src/main/java/io/netty/channel/ChannelId.java
@@ -42,15 +42,22 @@ import java.io.Serializable;
  * <li>{@code io.netty.processId} - an integer between 0 and 65535</li>
  * </ul>
  * </p>
+ *
+ *
+ * channel 编号
  */
 public interface ChannelId extends Serializable, Comparable<ChannelId> {
     /**
      * Returns the short but globally non-unique string representation of the {@link ChannelId}.
+     *
+     * 全局非唯一
      */
     String asShortText();
 
     /**
      * Returns the long yet globally unique string representation of the {@link ChannelId}.
+     *
+     * 全局唯一
      */
     String asLongText();
 }
